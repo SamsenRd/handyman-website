c7f9d840b28fa4d0e016f8755435bca767e51e91
SamanthaF <samantha.foigel@gmail.com>
2024-05-27 14:43:39 -0400

    hero section updates

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,15 @@
 import React from "react"
+import styles from "../styles/Hero.module.css"
+import heroImage from "../assets/images/hero3.jpeg"
 
 export default function Hero(){
     return(
-        <div>
-
-        </div>
+        <>
+            <div className={styles.heroContainer}>
+                <img src={heroImage} className={styles.img} alt="hero-img" />
+                <h1 className={styles.heroTitle}>Professional Handyman Services</h1>
+                <button className={styles.heroButton}>Get a Quote!</button>
+            </div>
+        </>
     )
 }

345803946024f43267eddc8639f18a737f769940
SamanthaF <samantha.foigel@gmail.com>
2024-05-30 16:51:59 -0400

    style updates

--- src/components/WhoWeAre.js
+++ src/components/WhoWeAre.js
@@ -2,6 +2,9 @@ import React from "react"
 import styles from "../styles/WhoWeAre.module.css"
 import handymanImg from "../assets/images/handymanShot.jpeg"
 import skilled from "../assets/images/logical-thinking.png"
+import quick from "../assets/images/time.png"
+import tools from "../assets/images/tools.png"
+import trust from "../assets/images/trust.png"
 
 export default function WhoWeAre(){
     return(
@@ -17,10 +20,36 @@ export default function WhoWeAre(){
                         </div>
                         <p>We have a team of trained and skilled handymen and they all are reliable, trustworthy, dedicated, experienced, and background checked.</p>
                     </div>
+
+                    <div className={styles.WhoWeAreSkills}>
+                        <div className={styles.WhoWeAreSkillsInfo}> 
+                            <img src={quick} className={styles.WhoWeAreSkillsImg} alt="quick response icon" />  
+                            <h3>Fast response</h3>
+                        </div>
+                        <p>We have a team of trained and skilled handymen and they all are reliable, trustworthy, dedicated, experienced, and background checked.</p>
+                    </div>
+
+                    <div className={styles.WhoWeAreSkills}>
+                        <div className={styles.WhoWeAreSkillsInfo}> 
+                            <img src={tools} className={styles.WhoWeAreSkillsImg} alt="Tools icon" />  
+                            <h3>Well-Equipped</h3>
+                        </div>
+                        <p>We have a team of trained and skilled handymen and they all are reliable, trustworthy, dedicated, experienced, and background checked.</p>
+                    </div>
+
+                    <div className={styles.WhoWeAreSkills}>
+                        <div className={styles.WhoWeAreSkillsInfo}> 
+                            <img src={trust} className={styles.WhoWeAreSkillsImg} alt="Trusted technicians" />  
+                            <h3>Experienced & Able </h3>
+                        </div>
+                        <p>We have a team of trained and skilled handymen and they all are reliable, trustworthy, dedicated, experienced, and background checked.</p>
+                    </div>
                 </div>
 
                 <div className={styles.WhoWeAreImg}>
                     <img src={handymanImg} className={styles.handymanMainImg} alt="Tools" />
+                    <h2>Reliable Handyman Team</h2>
+                    <p>We are a reliable team of handymen dedicated to delivering quality services with passion.</p>
                 </div>
             </div>
         </>
